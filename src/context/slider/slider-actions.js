ae8ab73c75724d2246412e9ad5a25bb3ade4fa70
Pedro Marti <pedro.marti89@live.com>
2021-02-26 20:34:29 -0400

    added: reset fuctionality in Slider Component.

--- src/context/slider/slider-actions.js
+++ src/context/slider/slider-actions.js
@@ -2,6 +2,9 @@ const actions = {
   changeValue(dispatch, payload) {
     dispatch({ type: "change", payload });
   },
+  reset(dispatch) {
+    dispatch({ type: "reset" });
+  },
 };
 
 export default actions;
--- src/context/slider/slider-reducer.js
+++ src/context/slider/slider-reducer.js
@@ -6,6 +6,8 @@ export const reducer = (state, { type, payload }) => {
   switch (type) {
     case "change":
       return { ...state, count: payload };
+    case "reset":
+      return { ...initialState };
     default: {
       console.log(`Unhandled action type: ${type}`);
       return state;
